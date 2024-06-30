e1ba3df9d241f178159afc0b574cca91e979b8c0
Dzigsamsams <dzigsams@gmail.com>
2024-06-30 11:28:32 +0000

    Json

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
-
 COURCES = [
   {
-    'id': 1,
-    'title': 'Practical Python Programming',
-    'Cost for 6months': '8,000',
-    'Cost for 1yr': '14,000'
+      'id': 1,
+      'title': 'Practical Python Programming',
+      'Cost for 6months': '8,000',
+      'Cost for 1yr': '14,000'
   },
   {
-    'id': 2,
-    'title': 'Practical Java Programming',
-    'Cost for 6months': '6,000',
-    'Cost for 1yr': '11,000'
+      'id': 2,
+      'title': 'Practical Java Programming',
+      'Cost for 6months': '6,000',
+      'Cost for 1yr': '11,000'
   },
   {
-  'id': 3,
-  'title': 'Practical C# Programming',
-  'Cost for 6months': '7,000',
-  'Cost for 1yr': '12,000'
+    'id': 3,
+    'title': 'Practical C# Programming',
+    'Cost for 6months': '7,000',
+    'Cost for 1yr': '12,000'
   },
   {
-  'id': 4,
-  'title': 'Practical C++ Programming',
-  'Cost for 6months': '9,000',
-  'Cost for 1yr': '15,000'
+    'id': 4,
+    'title': 'Practical C++ Programming',
+    'Cost for 6months': '9,000',
+    'Cost for 1yr': '15,000'
   }
 ]
-
 @app.route("/")
-def hell0_world():
+def hello_world():
   return render_template('home.html', 
-                         cources=COURCES)
+                          cources=COURCES)
 
+@app.route("/api/cources")
+def list_cources():
+  return jsonify(COURCES)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+  
